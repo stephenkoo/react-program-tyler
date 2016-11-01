c678a7f2005c439e768de6525fa73c3db51d0693
Stephen Koo <stephen.koo@inlight.com.au>
2016-11-01 23:12:26 +1100

    Additional branch & tags to Sentry

--- app/index.js
+++ app/index.js
@@ -7,7 +7,19 @@ let sentryKey = '73f04bab20e84d418eff42efdc64a54f'
 let sentryApp = '111055'
 let sentryURL = `https://${sentryKey}@sentry.io/${sentryApp}`
 
-Raven.config(sentryURL).install()
+let _APP_INFO = {
+  name: 'Github Battle',
+  branch: '4',
+  version: '1.0'
+}
+
+Raven.config(sentryURL, {
+  release: _APP_INFO.version, // This lets you track which version of your App is causing error in Sentry's dashboard
+  tags: {
+    branch: _APP_INFO.branch, // This shows the branch causing error too.
+    github_commit: 'asdio293sd3i' // A common one. This shows the Github commit hash that caused error.
+  }
+}).install()
 
 console.log(window.thing.doesNotExist) // This error is viewable in Sentry's dashboard
 
