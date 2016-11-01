91e168e95190d76071ca2374c6f6cbd0227ca3ab
Stephen Koo <stephen.koo@inlight.com.au>
2016-11-01 23:06:48 +1100

    Set up Sentry & Raven

--- app/index.js
+++ app/index.js
@@ -1,7 +1,15 @@
 let React = require('react');
 let ReactDOM = require('react-dom');
 let routes = require('./config/routes');
+let Raven = require('raven-js')
 
+let sentryKey = '73f04bab20e84d418eff42efdc64a54f'
+let sentryApp = '111055'
+let sentryURL = `https://${sentryKey}@sentry.io/${sentryApp}`
+
+Raven.config(sentryURL).install()
+
+console.log(window.thing.doesNotExist) // This error is viewable in Sentry's dashboard
 
 ReactDOM.render(
   routes,
