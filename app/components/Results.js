fd6949ab948b62813cd89c2da67057c3151959a8
Stephen Koo <stephen.koo@inlight.com.au>
2016-11-22 11:25:45 +1100

    Add tie start-over button

--- app/components/Results.js
+++ app/components/Results.js
@@ -3,11 +3,27 @@ let PropTypes = React.PropTypes;
 let styles = require('../styles');
 let UserDetails = require('./UserDetails');
 let UserDetailsWrapper = require('./UserDetailsWrapper');
+let Link = require('react-router').Link;
 
-function puke (obj) {
-  return <pre>{JSON.stringify(obj, 2, ' ')}</pre>
-}
+// function puke (obj) {
+//   return <pre>{JSON.stringify(obj, 2, ' ')}</pre>
+// }
+
 function Results (props) {
+  // Checks for tie
+  if (props.scores[0] === props.scores[1]) {
+    return (
+      <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+        <h1>It's a tie!</h1>
+        <div className='col-sm-12' style={styles.space}>
+          <Link to='/playerOne'>
+            <button type='button' className='btn btn-lg btn-danger'>Start Over</button>
+          </Link>
+        </div>
+      </div>
+    )
+  }
+
   let winningIndex = props.scores[0] > props.scores[1] ? 0 : 1;
   let losingIndex = winningIndex === 0 ? 1 : 0;
   return {
@@ -21,6 +37,11 @@ function Results (props) {
         <UserDetailsWrapper header='Loser'>
           <UserDetails score={pros.scores[losingIndex]} info={props.playerInfor[losingIndex]} />
         </UserDetailsWrapper>
+      </div>
+      <div className='col-sm-12' style={styles.space}>
+        <Link to='/playerOne'>
+          <button type='button' className='btn btn-lg btn-danger'>Start Over</button>
+        </Link>
       </div>
     </div>
   }
